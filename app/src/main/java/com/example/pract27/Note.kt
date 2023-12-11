e74d22aea2e02151a0b94d420ece3820ca6fcaa0
Houdini <davletshin2004@mail.ru>
2023-12-11 17:37:43 +0300

    Updated Note.kt

--- app/src/main/java/com/example/pract27/Note.kt
+++ app/src/main/java/com/example/pract27/Note.kt
@@ -7,24 +7,26 @@ import androidx.annotation.RequiresApi
 import com.google.gson.Gson
 import java.time.LocalDateTime
 
-class Note (var title: String, var text : String) : Parcelable {
+class Note (
+    var title: String,
+    var text : String,
+    val id: Int = -1,
+    val creationTime: LocalDateTime = LocalDateTime.now()) : Parcelable {
     @RequiresApi(Build.VERSION_CODES.O)
-    var creationTime: LocalDateTime = LocalDateTime.now()
-        private set
 
-    private constructor(parcel: Parcel) : this(
+   private constructor(parcel: Parcel) : this(
         title = parcel.readString() ?: "none",
-        text = parcel.readString() ?: "none"
-    ) {
-        creationTime = LocalDateTime.parse(parcel.readString())
-    }
+        text = parcel.readString() ?: "none",
+        id = parcel.readInt() ?: -1,
+        creationTime = LocalDateTime.parse(parcel.readString()))
+
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(title)
         parcel.writeString(text)
+        parcel.writeInt(id)
         parcel.writeString(creationTime.toString())
     }
     override fun describeContents(): Int = 0
-
     companion object CREATOR : Parcelable.Creator<Note> {
         override fun createFromParcel(parcel: Parcel): Note {
             return Note(parcel)
